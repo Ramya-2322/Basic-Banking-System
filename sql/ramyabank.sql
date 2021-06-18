5f5cc6800b17e1b46f78f623ec15481f3cdc9480
Ramya-2322 <68776103+Ramya-2322@users.noreply.github.com>
2021-06-18 23:14:30 +0530

    initial commit

---
+++ sql/ramyabank.sql
@@ -0,0 +1,87 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO 'users' ('id', 'name', 'email', 'balance') VALUES
+(1, 'Ramya', 'ramya@gmail.com', 50000),
+(2, 'rachana', 'rachana@gmail.com', 30000),
+(3, 'girish', 'girish@gmail.com', 40000),
+(4, 'joseph', 'joseph@gmail.com', 50000),
+(5, 'david', 'david@gmail.com', 40000),
+(6, 'immaneual', 'immaneual@gmail.com', 30000),
+(7, 'prerana', 'prerana@gmail.com', 50000),
+(8, 'navaneetha', 'navaneetha@gmail.com', 40000),
+(9, 'vishal', 'vishal@gmail.com', 30000),
+(10, 'teju', 'teju@gmail.com', 50000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+
+
+
+
